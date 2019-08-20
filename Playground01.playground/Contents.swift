d8debeb7d7286e63eaae31ef194139a0682082db
developeration1 <42705200+developeration1@users.noreply.github.com>
2019-08-19 21:42:49 -0500

    Add files via upload

---
+++ Playground01.playground/Contents.swift
@@ -0,0 +1,8 @@
+import Cocoa
+
+var v1 = "Esto es Swift"
+let c1 = 4
+var cadena:String = String()
+cadena = "Hola Mundo " + v1
+print("El contenido es: \(cadena)")
+print("El valor de constante es: \(c1)")
